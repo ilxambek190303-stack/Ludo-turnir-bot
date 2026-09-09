eb98253571eff5db9f9a39e4b71dd76fe1afb8dd
ilxambek190303-stack <ilxambek190303@icloud.com>
2026-09-09 21:20:27 +0300

    Create models.py

---
+++ database/models.py
@@ -0,0 +1,7 @@
+cd ludo-tournament-bot
+git init
+git add .
+git commit -m "Ludo turnir bot"
+git remote add origin https://github.com/SIZNING_USERNAME/ludo-tournament-bot.git
+git branch -M main
+git push -u origin main
