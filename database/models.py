eb7dd1d04ee53dab6519033dcc9671d114148114
ilxambek190303-stack <ilxambek190303@icloud.com>
2026-09-10 13:13:37 +0300

    Delete database/models.py

--- database/models.py
+++
@@ -1,7 +0,0 @@
-cd ludo-tournament-bot
-git init
-git add .
-git commit -m "Ludo turnir bot"
-git remote add origin https://github.com/SIZNING_USERNAME/ludo-tournament-bot.git
-git branch -M main
-git push -u origin main
